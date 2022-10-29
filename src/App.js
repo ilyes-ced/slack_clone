bde7a898ab42154719ccc4f264fb7c6fddebeeaa
ilyes-guy <ilyes.guy@gmail.com>
2022-10-29 11:02:36 +0100

    ef

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Comp from './Comp'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+    return(
+        <div className='first'>
+            hi dude
+            <Comp title='titl name prop'/>
+        </div>
+    );
 }
+
+
+
+
+
+
 
 export default App;
---
+++ src/Comp.js
@@ -0,0 +1,21 @@
+function Comp(props) {
+    return(
+        <div className='hi'>
+            <ul>
+                <li>{props.title}</li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+export default Comp;

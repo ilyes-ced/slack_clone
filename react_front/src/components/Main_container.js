7c3910e36552a3faa65d1e4672b75371f72ebfcc
ilyes-guy <ilyes.guy@gmail.com>
2022-11-05 11:44:11 +0000

    4

--- react_front/src/components/Main_container.js
+++ react_front/src/components/Main_container.js
@@ -1,8 +1,13 @@
+import React, { useState } from "react";
 function Main_container() {
-    const fetchData = () => {
+
+    
+    const [data, set_data] = useState([]);
+    const fetchData =  () => {
         return fetch("http://localhost:5000")
             .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => {set_data(data)
+            console.log(data)});
     }
     fetchData()
     const keykey = (e)=>{
@@ -25,6 +30,7 @@ function Main_container() {
                     submit
                 </button>
             </form>
+            {data.forEach(element => <li>data.name</li>)}
         </div>
     )
 }

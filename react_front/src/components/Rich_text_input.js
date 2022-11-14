35e09ef07e91579fbfc04b9dc7a556352deecf5f
ilyes-guy <ilyes.guy@gmail.com>
2022-11-14 22:01:27 +0100

    zd

--- react_front/src/components/Rich_text_input.js
+++ react_front/src/components/Rich_text_input.js
@@ -1,29 +1,46 @@
-
-
-
+import { useState, useRef, createElement } from 'react'
 
 
 function Rich_text_input() {
-    const calcHeight = (value) => {
-        let numberOfLineBreaks = (value.match(/\n/g) || []).length;
-        let newHeight = 20 + numberOfLineBreaks * 20 + 12 + 2;
-        if(newHeight < 300) return newHeight;
-      }
-    const textarea_size = (e) => {
-        e.target.style.height = calcHeight(e.target.value) + "px";
+    const text_input = useRef(null)
+    const [is_disabled, change_ability] = useState(false)
+    const text_value = [{type: 'strong',content:'hello boldy'}, {type: 'i',content:'italia'}]
+    var vv =    ''
+    for(let i=0; i< text_value.length; i++){
+        vv = vv + '<'+text_value[i].type+'>'+text_value[i].content+'</'+text_value[i].type+'>'
     }
+    console.log(vv)
 
 
+    const clicked = (e) => {
+        if(e.target.innerText == 'bold'){
+            e.target.style.backgroundColor = 'green' === 'green' ? '' : 'green'
+        }
+    }
 
     return(
         <div id='rich_text_input'>
             <div id='rich_text_input_content'>
                 <div id='rich_text_top_icon_bar'>
-                    icons here
+                    <button disabled={is_disabled} onClick={clicked} className='text_icons'>bold</button>
+                    <button disabled={is_disabled} onClick={clicked} className='text_icons'>italic</button>
+                    <button disabled={is_disabled} onClick={clicked} className='text_icons'>line over</button>
+                    <button disabled={is_disabled} onClick={clicked} className='text_icons'>link</button>
+                    <button disabled={is_disabled} onClick={clicked} className='text_icons'>list</button>
+                    <button disabled={is_disabled} onClick={clicked} className='text_icons'>numbered list</button>
+                    <button disabled={is_disabled} onClick={clicked} className='text_icons'>quote</button>
+                    <button disabled={is_disabled} onClick={clicked} className='text_icons'>code</button>
+                    <button disabled={is_disabled} onClick={clicked} className='text_icons'>code block</button>
                 </div>
-                <textarea onKeyUp={textarea_size} name="" id="rich_text_field" cols="30" rows="10"></textarea>
+                <div contenteditable="true" id='rich_text_field' dangerouslySetInnerHTML={{ __html: vv }}></div>
+                {/* {onBlur={() => {change_ability(true)} 
+                <textarea  ref={text_input} onKeyUp={textarea_size} onFocus={() => {change_ability(false)}}  name="" ></textarea>
+                */}
                 <div id='rich_text_bottom_icon_bar'>
                     icons here
+                    <div id='submit_text'>
+                        send_icon
+                    </div>
                 </div>
             </div>
         </div>

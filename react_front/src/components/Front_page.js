253c191db6d75a1553e6a20082307caa656644df
ilyes-guy <ilyes.guy@gmail.com>
2023-01-03 18:19:18 +0100

    zd

--- react_front/src/components/Front_page.js
+++ react_front/src/components/Front_page.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react'
 import { Navigate, useNavigate } from 'react-router-dom';
-
-
+import { BsSearch } from "react-icons/bs";
 
 function Front_page(props) {
     const nav = useNavigate()
     const [is_auth, set_is_auth] = useState('none')
     const [show_page, set_show_page] = useState(false)
     const [workspaces, set_workspaces] = useState([])
+    const [show_input, set_show_input] = useState(false)
 
     fetch(process.env.REACT_APP_API_URL+"/users/verify_user", {
         method: 'POST',
@@ -55,7 +55,22 @@ function Front_page(props) {
 
 
             <nav id='front_nav'>
-                nav here idk style
+                <div>
+                    <div> <img src="/logo192.png" alt="" /> </div>
+                    <div>random</div>
+                    <div>random</div>
+                    <div>random</div>
+                    <div>random</div>
+                    <div>random</div>
+                </div>
+                <div>
+                    <div>
+                        <input placeholder='Search . . .' type="text" style={show_input ? {width: "100%", border: "1px solid white", padding: '10px' } : {width: "0px", border: 'none', padding: '0px' } }  />
+                        <BsSearch onClick={() => { set_show_input(!show_input) }} />
+                    </div>
+                    <div>Create workspace</div>
+                    <div>Information</div>
+                </div>
             </nav>
 
 
@@ -83,11 +98,10 @@ function Front_page(props) {
             </div>
 
 
-
+            {/*
             <div id="front_div2"> buncha useless garbage here </div>
-
-
             <div id="front_footer">fotter here</div>
+            */}
         </div>
     )
     return <div id='loader_parent'><div className="loader"></div>
